26c21bfe0d9e2849aa9fb29e5645e5d2aa53b3ed
Mayank Kapoor <mkapoor@Mayanks-MacBook-Pro.local>
2014-11-08 11:28:14 +0000

    Initial Commit

---
+++ FirstTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTest
+//
+//  Created by Mayank Kapoor on 08/11/2014.
+//  Copyright (c) 2014 Vasile Enterprise Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
